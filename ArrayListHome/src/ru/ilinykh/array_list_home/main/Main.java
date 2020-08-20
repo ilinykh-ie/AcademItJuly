8f3f06689301767dcb4c38c08d382b35cdd46769
ilinykh-ie <ilinykh-ie@mail.ru>
2020-08-20 16:41:54 +0700

    Исправление задачи ArrayListHome.

--- ArrayListHome/src/ru/ilinykh/array_list_home/main/Main.java
+++ ArrayListHome/src/ru/ilinykh/array_list_home/main/Main.java
@@ -12,12 +12,7 @@ public class Main {
             return null;
         }
 
-        if (list.size() == 0) {
-            return new ArrayList<>();
-        }
-
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(list.get(0));
+        ArrayList<Integer> result = new ArrayList<>(list.size());
 
         for (Integer e : list) {
             if (!result.contains(e)) {
@@ -41,12 +36,7 @@ public class Main {
 
         System.out.println("Список из файла: " + listFromFile);
 
-        ArrayList<Integer> integerList = new ArrayList<>();
-
-        for (int i = 0; i < 15; i++) {
-            integerList.add(i);
-        }
-
+        ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(0, -5, 18, 12, 3, 1, -10, 0, 18, 20, -3));
         System.out.println("Список целых чисел: " + integerList);
 
         for (int i = 0; i < integerList.size(); i++) {
@@ -58,9 +48,10 @@ public class Main {
 
         System.out.println("Список без четных чисел: " + integerList);
 
-        ArrayList<Integer> integerList2 = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 5, 5, 8, 7, 7, 7, 1, 1, 3, 3));
-        System.out.println("Произвольный список: " + integerList2);
-        ArrayList<Integer> integerList3 = getListWithoutRepeats(integerList2);
-        System.out.println("Список без повторяющихся элементов: " + integerList3);
+        ArrayList<Integer> randomList = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 5, 5, 8, 7, 7, 7, 1, 1, 3, 3));
+        System.out.println("Произвольный список: " + randomList);
+
+        ArrayList<Integer> listWithoutRepeats = getListWithoutRepeats(randomList);
+        System.out.println("Список без повторяющихся элементов: " + listWithoutRepeats);
     }
 }
